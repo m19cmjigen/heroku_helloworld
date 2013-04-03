ae4133cea2bad405b5413886011c7943b58ef0ab
m19cmjigen <m19cmjigen@gmail.com>
2013-04-04 01:55:39 +0900

    add three.js

--- web.js
+++ web.js
@@ -35,3 +35,24 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+app.get('/shop', function(req, res) {
+  var http = require('http');
+  
+  var host= 'api.doko.jp';
+  var dir = '/v1/searchPOI.do?key=&lt;取得したキー&gt;&amp;format=json';  // 今回はJSONを使います
+
+  var json = '';
+  var request = http.createClient(80, host).request('GET', dir, {'HOST': host});
+  request.addListener('response', function(response) {
+    response.setEncoding('utf-8');
+    response.addListener('data', function(chunk) {
+      json += chunk;
+    });
+    response.addListener('end', function() {
+      var results = JSON.parse(json);
+      res.render('shop', {title: 'Shop', results: results});
+    });
+  });
+  request.end();
+});
