9bf87eca495037a4821b8723ccda04ca475a9743
m19cmjigen <m19cmjigen@gmail.com>
2015-02-03 22:18:01 +0900

    add oyaji

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,16 +3,23 @@ var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHe
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-var geometry = new THREE.CubeGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+// var geometry = new THREE.CubeGeometry(1,1,1);
+// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+var material = new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('/images/oyaji.jpg')});
+var geometry = new THREE.PlaneGeometry(200, 200);
+// var material = THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture('/images/oyaji.jpg')});
+// var material = new THREE.MeshPhongMaterial( { map: THREE.ImageUtils.loadTexture('images/oyaji.jpg') } );
 var cube = new THREE.Mesh( geometry, material );
+cube.overdraw=true;
 scene.add( cube );
-camera.position.z = 5;
+camera.position.z = 500;
 
 function render() {
         requestAnimationFrame(render);
-        cube.rotation.x += 0.1;
-        cube.rotation.y += 0.1;
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
         renderer.render(scene, camera);
 }
 render();
