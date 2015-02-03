b7758b1ae64467a3ac2caed735ecb78d5e8414c2
m19cmjigen <m19cmjigen@gmail.com>
2015-02-03 22:32:33 +0900

    傾きで動くように

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -16,11 +16,34 @@ cube.overdraw=true;
 scene.add( cube );
 camera.position.z = 500;
 
-function render() {
-        requestAnimationFrame(render);
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-        renderer.render(scene, camera);
+function render(x, y) {
+//    requestAnimationFrame(render);
+    cube.rotation.x += x;
+    cube.rotation.y += y;
+    renderer.render(scene, camera);
 }
-render();
+render(0, 0);
+
+/* debug 
+for( i = 0 ; i<100 ; i++) {
+    setInterval( function(){ render(i, i);}, 500);
+}
+*/
+
+	window.addEventListener("devicemotion", function(event){
+		ctx.clearRect(0, 0, 300, 400);
+		var x_a = event.acceleration.x;
+		var y_a = event.acceleration.y;
+		var z_a = event.acceleration.z;
+		var x_g = event.accelerationIncludingGravity.x;
+		var y_g = event.accelerationIncludingGravity.y;
+		var z_g = event.accelerationIncludingGravity.z;
+		ctx.fillText("X : "+x_a, 20, 20);
+		ctx.fillText("Y : "+y_a, 20, 40);
+		ctx.fillText("Z : "+z_a, 20, 60);
+		ctx.fillText("X傾き : "+x_g, 20, 80);
+		ctx.fillText("Y傾き : "+y_g, 20, 100);
+		ctx.fillText("Z傾き : "+z_g, 20, 120);
+        render(x_g, y_g);
+	}, true);
 
